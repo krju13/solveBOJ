3edf187138cc13e08c2dd950a9317e41e319f204
krju13 <krju13@gmail.com>
2021-06-03 19:46:40 +0900

    solve 1790

--- com/boj/week15_vacation/BOJ1790.java
+++ com/boj/week15_vacation/BOJ1790.java
@@ -1,2 +1,27 @@
-package com.boj.week15_vacation;public class BOJ1790 {
+package com.boj.week15_vacation;
+
+import java.util.Scanner;
+
+public class BOJ1790 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int K=sc.nextInt();
+        int place;
+        for(int i=1;i<=N;i++){
+            //i의 자릿수 구하기
+            place= (int) Math.log10(i)+1;
+            if(K<=place){
+                char num=Integer.toString(i).charAt(K-1);
+                System.out.println(num);
+                K=0;
+                break;
+            }else{
+                K-=place;
+            }
+        }
+        if(K!=0){
+            System.out.println(-1);
+        }
+    }
 }
--- com/boj/week15_vacation/BOJ2658.java
+++ com/boj/week15_vacation/BOJ2658.java
@@ -1,2 +1,7 @@
-package com.boj.week15_vacation;public class BOJ2658 {
+package com.boj.week15_vacation;
+
+public class BOJ2658 {
+    public static void main(String[] args) {
+
+    }
 }
